a38481452a5cd6b1f9be93d264d95237be055596
github username <github email address>
2019-12-13 10:58:00 +0000

    initial commit with searchbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    currentWord: '',
+    definition: '',
+    etymology: '',
+    data: ''
+  }
+
+  fetchIt = (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:3000/bot', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }, body:
+        JSON.stringify({ word: this.state.currentWord })
+    })
+      .then(res => res.json())
+      .then(results => this.setState({
+        definition: results['word']['definition'],
+        etymology: results['word']['etymology'],
+        data: results['word']['frequency']
+      }))
+
+  }
+
+  handleChange = (e) => {
+    this.setState({ currentWord: e.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.fetchIt}>
+          <div className="ui input focus">
+            <input onChange={this.handleChange} placeholder="Search"></input>
+            <button className='ui button' type='submit' >Search</button>
+          </div>
+          <p>{this.state.definition} </p>
+          <p>{this.state.etymology} </p>
+          <p>{this.state.data} </p>
+        </form>
+
+      </div>
+    );
+  }
 }
 
 export default App;
